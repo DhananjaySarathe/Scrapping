369a28138981dabb69dc4e8fe2f8b26ac04fd846
Dhananjay Sarathe <dhananjay.s@quickads.ai>
2025-11-08 23:13:57 +0530

    Update HTML retrieval script and add Flipkart product page HTML file. Changed user agent import and updated request headers. The URL has been modified to target a specific product page, and a new HTML file for the product has been created.

--- 0_retriving_html.py
+++ 0_retriving_html.py
@@ -1,20 +1,21 @@
 import requests
 import time
-from fake_useragents import UserAgent
+from fake_useragent import UserAgent
 
-url = "https://www.flipkart.com/audio-video/headset/pr?sid=0pm%2Cfcn&p%5B%5D=facets.connectivity%255B%255D%3DBluetooth&sort=popularity&p%5B%5D=facets.rating%255B%255D%3D3%25E2%2598%2585%2B%2526%2Babove&p%5B%5D=facets.rating%255B%255D%3D4%25E2%2598%2585%2B%2526%2Babove&p%5B%5D=facets.price_range.from%3D599&p%5B%5D=facets.price_range.to%3DMax&p%5B%5D=facets.headphone_type%255B%255D%3DTrue%2BWireless&param=86&hpid=WqCPtE2MbDEYEbYbttxC1qp7_Hsxr7nj65vMAAFK1c%3D&ctx=eyJjYXQiOiJ1ZGV4dCI6eyJhdHRyaWJ1dGVzIjp7InZhbHVlIjoiVkFMVUVFQ0FMTElOVCJ9fX0%3D"
+url = "https://www.flipkart.com/triggr-trinity-2-dual-pairing-enc-fast-charge-50h-battery-rubber-finish-v5-3-bluetooth/p/itm03c651cfe2be4?pid=ACCH8Q2VGN7MHYHS&lid=LSTACCH8Q2VGN7MHYHSQSLI2G&marketplace=FLIPKART&store=0pm%2Ffcn%2Fgc3&srno=b_1_1&otracker=browse&fm=organic&iid=en_MYwYZQAvTrJ57LucwBti_yihZ-SGd6RV9w3EoZk5sdA7BdVINqLiWmBKR62_2uAPGmY7UH3-UJ0m5SfxA-TWWPUFjCTyOHoHZs-Z5_PS_w0%3D&ppt=browse&ppn=browse&ssid=07l2rjdu4g0000001762623596799"
 
 session = requests.Session()
-session.headers.update({
+headers = {
     "User-Agent": UserAgent().random,
     "accept-language": "en-US,en;q=0.9",
     "Accept-encoding": "gzip, deflate, br",
     "Connection": "keep-alive",
     "Referer": "https://www.google.com/",
-})
+}
 
 # r=requests.get(url, headers=session.headers)   //Abhi request se maari toh session use ni hua and loader hi aa gya baha.. Now we have to take request using session ....
-r=session.get(url, headers=session.headers)
+time.sleep(2)
+r=session.get(url, headers=headers)
 
 with open("flipkart.html", "w") as f:
     f.write(r.text)
